6ca5870f5944cf8100e5b32dd40e49b469c19057
unknown <pawan.sviet@gmail.com>
2023-05-27 23:42:36 +0530

    BT using recursiion in process

---
+++ BinaryTree_using_recursion.py
@@ -0,0 +1,107 @@
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+        
+    def insert(self, current, data):
+        # print("curret: ",current)
+        
+        if current == None:
+            return Node(data)
+            
+        if data < current.data:
+            current.left= self.insert(current.left,data)
+    
+        else:
+           current.right= self.insert(current.right,data)
+           
+        return current
+   
+    
+    def searchNode(self,current,data):
+        
+        if current is None:
+            return False
+        elif current.data == data:
+            return True
+    
+        #LEFT  
+        elif data < current.data:
+            return self.searchNode(current.left,data)
+        #RIGHT    
+        else:
+            return self.searchNode(current.right,data)
+            
+    
+    def delZeroChild(self, current, data):
+        
+        if current == None:
+            print("Not found")
+            
+        elif current.left == None and current.right == None and current.data == data:
+            current = None
+            return current
+            
+        elif data < current.data:
+            return self.delZeroChild(current.left, data)
+            
+        else:
+            prev_current = current.right
+            return self.delZeroChild(current.right, data)
+            
+        
+
+            
+                
+                
+            
+            
+bt1 = BinaryTree()
+
+bt1.root = bt1.insert(bt1.root, 100)
+bt1.insert(bt1.root, 90)
+bt1.insert(bt1.root, 80)
+bt1.insert(bt1.root, 95)
+bt1.insert(bt1.root, 85)
+# bt1.insert(bt1.root, 70)
+# bt1.insert(bt1.root, 110)
+# bt1.insert(bt1.root, 145)
+# bt1.insert(bt1.root, 23)
+# bt1.insert(bt1.root, 115)
+# bt1.insert(bt1.root, 345)
+
+# print(bt1.searchNode(bt1.root,15))
+
+# print(bt1.root.left.right.right)
+
+bt1.delZeroChild(bt1.root, 85)
+
+print(bt1.root.left.left.right.data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
