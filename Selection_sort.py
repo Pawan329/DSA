c25e202ba7448af5ef3e3e2c971b07b29ea9b83b
unknown <pawan.sviet@gmail.com>
2023-07-16 20:49:41 +0530

    Code modified

--- Selection_sort.py
+++ Selection_sort.py
@@ -11,15 +11,18 @@ and moved to the sorted subarray.
 '''
 
 
-data = [5, 4, 3, 2, 1]
+data = [10, 6, 12, 14, 2, 21, 25]
 
-for j in range(len(data)-1):
-    for i in range(j, len(data)-1):
-        if data[i] > data[i+1]:
-            min_index = i+1
-
-    temp = data[j]
-    data[j] = data[min_index]
+for i in range(len(data)-1):
+    
+    min_index = i
+    
+    for j in range(i+1, len(data)-2):
+        if data[j] < data[i]:
+            min_index = j
+    
+    temp = data[i]
+    data[i] = data[min_index]
     data[min_index] = temp
     
 print(data)
